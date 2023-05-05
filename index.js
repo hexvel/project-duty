a4d516fd6d8fd9999dd874db30efaa77c0f7af06
HEXVEL <dimaabdukarimov1234@gmail.com>
2023-05-05 21:58:31 +0300

    крч я хуй знает, добавил всё что смог придумтаь за 2 минуты

--- index.js
+++ index.js
@@ -3,12 +3,39 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/callback', (req, res) => {
-  const data = req.body;
-  console.log(data);
-  res.send('ok');
-});
+class Main {
 
-app.listen(5000, () =>
-  console.log('Сервер с портом 3000 запущен')
-);
+  constructor(api) {
+    this.api = api
+  }
+
+  genSecret(length) {
+    const chars = 'abdehkmnpswxzABDEFGHKMNPQRSTWXZ123456789';
+    let secret_key = '';
+    for (var i = 0; i < length; i++) {
+      var pos = Math.floor(Math.random() * chars.length);
+      secret_key += chars.substring(pos, pos + 1);
+    }
+    return secret_key;
+  }
+
+  server(port) {
+    app.post('/callback', (req, res) => {
+      const data = req.body;
+      res.send('ok');
+    });
+
+    app.get('/getKey/', (req, res) => {
+      const secret_key = this.genSecret(96)
+      res.send(`Ваш секретный ключ: ${secret_key}`)
+    })
+
+    app.listen(port, () =>
+      console.log('Сервер с портом 5000 запущен')
+    );
+  }
+
+}
+
+const _class = new Main("тест")
+_class.server(5000)
